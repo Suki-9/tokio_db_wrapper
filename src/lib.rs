6d7b6e4bc0aebadcfa13e0f50045bb7caebe1579
suki-9 <suki.music2004@gmail.com>
2024-02-12 22:00:27 +0900

    create other macro and fix

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,20 @@ pub fn run_query(query: &str, panic: bool) {
   }
 }
 
-pub fn run_query2(query: &str, panic: bool) -> Option<Vec<tokio_postgres::Row>> {
-  println!("[DB Service] Run query\n  ->{query:}");
+pub fn run_query2(query: &str) -> Option<Vec<tokio_postgres::Row>> {
+  println!("[DB Service] Run query\n  -> {query:}");
   match futures::executor::block_on((&client::DB_CLIENNT).query(query, &[])) {
     Ok(row) => Some(row),
-    Err(e) =>
-      if panic { panic!("{e:}") }
-      else { print!("{e:}"); None },
+    Err(e) => { print!("{e:}"); None },
   }
 }
 
-//TODO 文字列じゃなくてRustの形を受け取ってpostgre側の方に変換するようにすると読む時幸せになれるかも。
+//TODO autoincrementとかも使えるように分岐を使って分けるべき。
 #[macro_export]
 macro_rules! create_table {
   ($t:expr, $( ($k:expr, $tp:ty) ),+) => {
     let mut tmp: Vec<String> = Vec::new();
-    $(tmp.push(format!("{} {:?}", $k, 
+    $(tmp.push(format!("{} {}", $k, 
       match std::any::type_name::<$tp>() {
         "i32" | "i16" | "i8" | "isize" => "INTEGER",
         "i64" => "BIGINT",
@@ -32,6 +30,23 @@ macro_rules! create_table {
         "String" | "str" => "TEXT",
         _ => panic!("[DB Service] Type Error!!"),
       }
+    ));)*
+    println!("CREATE TABLE {} ({});", $t ,tmp.join(","));
+    $crate::run_query(&format!("CREATE TABLE {} ({});", $t ,tmp.join(",")), true);
+  };
+
+  ($t:expr, $( ($k:expr, $tp:ty, $op:expr) ),+) => {
+    let mut tmp: Vec<String> = Vec::new();
+    $(tmp.push(format!("{} {} {}",
+      $k, 
+      match std::any::type_name::<$tp>() {
+        "i32" | "i16" | "i8" | "isize" => "INTEGER",
+        "i64" => "BIGINT",
+        "bool" => "BOOL",
+        "u32" | "u16" | "u8" | "usize" => "FROAT8",
+        "String" | "str" => "TEXT",
+        _ => panic!("[DB Service] Type Error!!")},
+      $op
     ));)*
     println!("CREATE TABLE {} ({});", $t ,tmp.join(","));
     $crate::run_query(&format!("CREATE TABLE {} ({});", $t ,tmp.join(",")), true);
@@ -48,29 +63,44 @@ macro_rules! drop_table {
 #[macro_export]
 macro_rules! insert_record {
   ($t:expr, $( $k_t:expr ),+) => {
-    let mut tmp: Vec<String> = Vec::new();
-    $(tmp.push(format!("{} {}", $k_t.0, $k_t.1));)*
-    $crate::run_query(&format!("CREATE TABLE {} ({});", $t ,tmp.join(",")), false)
+    let mut keys: Vec<String> = Vec::new();
+    let mut values: Vec<String> = Vec::new();
+    $(
+      keys.push(format!("{}", $k_t.0));
+      values.push(format!("'{}'", $k_t.1));
+    )*
+    $crate::run_query(&format!("INSERT INTO {} ({}) VALUES({});", $t ,keys.join(","), values.join(",")), false)
   };
 }
 
 #[macro_export]
 macro_rules! select_record {
-  ($t:expr, $( $k_t:expr ),+) => {
+  ($t:expr, $( $c:expr ),+) => {{
     let mut tmp: Vec<String> = Vec::new();
-    $(tmp.push(format!("{} {}", $k_t.0, $k_t.1));)*
-    $crate::run_query(&format!("CREATE TABLE {} ({});", $t ,tmp.join(",")), false)
-  };
-}
+    $(tmp.push(format!("{}", $c));)*
+    $crate::run_query2(&format!("SELECT {} FROM {};", tmp.join(","), $t))
+  }};
 
+  ($t:expr, $op:expr, $( $c:expr ),+) => {{
+    let mut tmp: Vec<String> = Vec::new();
+    $(tmp.push(format!("{}", $c));)*
+    $crate::run_query2(&format!("SELECT {} FROM {} WHERE {};", tmp.join(","), $t, $op))
+  }};
+}
 
 #[macro_export]
 macro_rules! update_record {
-  ($t:expr, $( $k_t:expr ),+) => {
+  ($t:expr, $( ($k:expr, $v:expr) ),+) => {
     let mut tmp: Vec<String> = Vec::new();
-    $(tmp.push(format!("{} {}", $k_t.0, $k_t.1));)*
-    $crate::run_query(&format!("CREATE TABLE {} ({});", $t ,tmp.join(",")), false)
+    $(tmp.push(format!("{} = '{}'", $k, $v));)*
+    $crate::run_query(&format!("UPDATE {} SET {};", $t ,tmp.join(",")), false)
   };
+
+  ($t:expr, $op:expr, $( ($k:expr, $v:expr) ),+) => {{
+    let mut tmp: Vec<String> = Vec::new();
+    $(tmp.push(format!("{} = '{}'", $k, $v));)*
+    $crate::run_query(&format!("UPDATE {} SET {} WHERE {};", $t ,tmp.join(","), $op), false)
+  }};
 }
 
 #[macro_export]
